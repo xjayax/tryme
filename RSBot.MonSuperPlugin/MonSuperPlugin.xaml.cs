2c7caa590cb7d241e49d4aa198113811082ecb87
jaya <130752636+xjayax@users.noreply.github.com>
2025-07-12 18:41:10 +0200

    Add files via upload
    t1

---
+++ RSBot.MonSuperPlugin/MonSuperPlugin.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms.Integration;
+using WinFormsControl = System.Windows.Forms.Control;
+using RSBot.Core;
+using RSBot.Core.Plugins;
+
+namespace RSBot.MonSuperPlugin
+{
+    public partial class MonSuperPlugin : UserControl, IPlugin
+    {
+        private ElementHost host;
+
+        public MonSuperPlugin()
+        {
+            InitializeComponent();
+            // Liaisons automatiques (si non reliées dans le XAML)
+        }
+
+        // Propriétés RSBot
+        public string InternalName => "MonSuperPlugin";
+        public string DisplayName => "Mon Super Plugin";
+        public bool DisplayAsTab => true;
+        public int Index => 998;
+        public bool RequireIngame => false;
+
+        public WinFormsControl View
+        {
+            get
+            {
+                if (host == null)
+                    host = new ElementHost { Child = this };
+                return host;
+            }
+        }
+
+        public void Initialize()
+        {
+            Log.Notify($"[{DisplayName}] Plugin initialized!");
+            // Abonnement aux événements globaux du bot
+        }
+
+        public void Translate() { /* Pour la traduction automatique si besoin */ }
+        public void Dispose() { /* Detach events ici si besoin */ }
+
+        // Méthodes d'événements pour chaque contrôle (Voir exemple plus haut)
+        // ... (Insère ici toutes les méthodes comme UniqueTPJCave_Checked etc)
+    }
+}
---
+++ RSBot.MonSuperPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Informations générales sur l’assembly
+[assembly: AssemblyTitle("RSBot.MonSuperPlugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("ecodair")]
+[assembly: AssemblyProduct("RSBot.MonSuperPlugin")]
+[assembly: AssemblyCopyright("Copyright © ecodair 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// COM
+[assembly: ComVisible(false)]
+[assembly: Guid("14c5a690-2de5-49e3-b5f6-387e192e5387")]
+
+// Version
+
